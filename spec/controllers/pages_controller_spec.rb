5e80612f6638233d7dba96f28fe57d044470cd19
Lari Kirby <larikirby@fastmail.fm>
2011-04-11 14:16:35 -0400

    added help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -38,5 +38,17 @@ describe PagesController do
           :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+          :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
 
 end
