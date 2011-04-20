c33169467efc5d4bd4827a179c35566954ed709d
Lari Kirby <larikirby@fastmail.fm>
2011-04-20 15:13:20 -0400

    encrypt & save password, authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 # == Schema Information
-# Schema version: 20110417175447
+# Schema version: 20110420184334
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
+
+require 'digest'
 
 class User < ActiveRecord::Base
   attr_accessor :password
@@ -24,6 +28,39 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => { :within => 6..40 }
+  
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password. 
+  def has_password?(submitted_password) 
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+    
 end
 
 
