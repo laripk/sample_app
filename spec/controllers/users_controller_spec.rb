bf0afa7e11e14e698c002eac86ff157242b92c77
Lari Kirby <larikirby@fastmail.fm>
2011-04-20 22:42:22 -0400

    add flash message to create user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -93,6 +93,11 @@ describe UsersController do
         response.should redirect_to(user_path(assigns(:user)))
       end
       
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+      
     end
     
   end
