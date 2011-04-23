921478f01a3f051224507c695908e448c23e47e3
Lari Kirby <larikirby@fastmail.fm>
2011-04-23 09:35:12 -0400

    add profile link & integration test

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -65,7 +65,11 @@ describe "LayoutLinks" do
       response.should have_selector("a", :href => signout_path, :content => "Sign out")
     end
     
-    it "should have a profile link"
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
     
   end #signed in
 
