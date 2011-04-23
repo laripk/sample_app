44d0931cd8fbe95d85e11df3bc337041da5fe592
Lari Kirby <larikirby@fastmail.fm>
2011-04-23 09:45:50 -0400

    add sign in/out integration tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -66,11 +66,11 @@ describe "Users" do
         visit signin_path
         #print "user in db=", !User.find_by_email(@user.email).nil?
         #print User.count
-        @user = Factory(:user)
+        user = Factory(:user)
         #print "user in db=", !User.find_by_email(@user.email).nil?
         #print User.count
-        fill_in :email,    :with => @user.email
-        fill_in :password, :with => @user.password
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
         click_button
         #print controller.params.inspect
         #print flash.inspect
@@ -80,5 +80,36 @@ describe "Users" do
     end #success
     
   end #sign in
+  
+  describe "sign in/out" do
+    
+    describe "failure" do
+      
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email,    :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+      
+    end #failure
+    
+    describe "success" do
+      
+      it "should sign a user in and out" do
+        visit signin_path
+        user = Factory(:user)
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+      
+    end #success
+    
+  end #sign in/out
   
 end
