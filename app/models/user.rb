58c98dde0c96c2a24568601461b7d994742a0bac
Lari Kirby <larikirby@fastmail.fm>
2011-04-29 18:19:03 -0400

    added model for following

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", 
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id", 
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships #, :source => :follower
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -58,6 +65,18 @@ class User < ActiveRecord::Base
     # print " uas", User.count
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   def feed
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,55 +11,59 @@ describe User do
     }
   end
   
-  it "should create a new instance given valid attributes" do
-    User.create!(@attr)
-  end
+  describe "creation and validation" do
   
-  it "should require a name" do
-    no_name_user = User.new(@attr.merge(:name => ""))
-    no_name_user.should_not be_valid
-  end
-  
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
-  
-  it "should reject names that are too long" do
-    long_name = "a" * 51
-    long_name_user = User.new(@attr.merge(:name => long_name))
-    long_name_user.should_not be_valid
-  end
-  
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    it "should create a new instance given valid attributes" do
+      User.create!(@attr)
     end
-  end
   
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-    addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
+    it "should require a name" do
+      no_name_user = User.new(@attr.merge(:name => ""))
+      no_name_user.should_not be_valid
     end
-  end
   
-  it "should reject duplicate email addresses" do
-    # put a user with given email address into the database.
-    User.create!(@attr)
-    user_with_duplicate_email=User.new(@attr.merge(:name=>@attr[:name]+" 2"))
-    user_with_duplicate_email.should_not be_valid
-  end
+    it "should require an email address" do
+      no_email_user = User.new(@attr.merge(:email => ""))
+      no_email_user.should_not be_valid
+    end
   
-  it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
+    it "should reject names that are too long" do
+      long_name = "a" * 51
+      long_name_user = User.new(@attr.merge(:name => long_name))
+      long_name_user.should_not be_valid
+    end
+  
+    it "should accept valid email addresses" do
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        valid_email_user = User.new(@attr.merge(:email => address))
+        valid_email_user.should be_valid
+      end
+    end
+  
+    it "should reject invalid email addresses" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |address|
+        invalid_email_user = User.new(@attr.merge(:email => address))
+        invalid_email_user.should_not be_valid
+      end
+    end
+  
+    it "should reject duplicate email addresses" do
+      # put a user with given email address into the database.
+      User.create!(@attr)
+      user_with_duplicate_email=User.new(@attr.merge(:name=>@attr[:name]+" 2"))
+      user_with_duplicate_email.should_not be_valid
+    end
+  
+    it "should reject email addresses identical up to case" do
+      upcased_email = @attr[:email].upcase
+      User.create!(@attr.merge(:email => upcased_email))
+      user_with_duplicate_email = User.new(@attr)
+      user_with_duplicate_email.should_not be_valid
+    end
+  
+  end #creation and valids
   
   describe "password validations" do
     
@@ -155,5 +159,116 @@ describe User do
     
   end #admin
   
+  describe "micropost associations" do
+    
+    before(:each) do
+      @user = User.create(@attr)
+      @u1mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+      @u1mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+      u2 = User.create(@attr.merge(:email => "user2@example.com"))
+      @u2mp3 = Factory(:micropost, :user => u2)
+    end
+    
+    it "should have a microposts attribute" do
+      @user.should respond_to(:microposts)
+    end
+    
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [@u1mp2, @u1mp1]
+      @user.microposts.should_not include(@u2mp3) # I just felt the need to emphasise this
+    end
+    
+    it "should destroy associated microposts" do
+      @user.destroy
+      [@u1mp1, @u1mp2].each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+        # another way to test it, for reference
+        # lambda do
+        #   Micropost.find(micropost.id)
+        # end.should raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+    
+    describe "status feed" do
+      
+      it "should have a feed" do
+        @user.should respond_to(:feed)
+      end
+      
+      it "should include the user's microposts" do
+        @user.feed.include?(@u1mp1).should be_true
+        @user.feed.include?(@u1mp2).should be_true
+      end
+      
+      it "should not include a different user's microposts" do
+        @user.feed.include?(@u2mp3).should be_false
+      end
+      
+    end #feed
+    
+  end #microposts assoc
+  
+  describe "relationships" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+      @followed = Factory(:user)
+    end
+    
+    it "should have a relationships method" do
+      @user.should respond_to(:relationships)
+    end
+    
+    it "should have a following method" do
+      @user.should respond_to(:following)
+    end
+    
+    it "should have a following? method" do
+      @user.should respond_to(:following?)
+    end
+    
+    it "should have a follow! method" do
+      @user.should respond_to(:follow!)
+    end
+    
+    it "should follow another user" do
+      @user.follow!(@followed)
+      @user.should be_following(@followed)
+    end
+    
+    it "should include the followed user in the following array" do
+      @user.follow!(@followed)
+      @user.following.should include(@followed)
+    end
+    
+    it "should have an unfollow! method" do
+      @followed.should respond_to(:unfollow!)
+    end
+    
+    it "should unfollow a user" do
+      @user.follow!(@followed)
+      @user.unfollow!(@followed)
+      @user.should_not be_following(@followed)
+    end
+    
+    it "should fail unfollowing a not-followed user" do
+      @user.should_not be_following(@followed)
+      expect{ @user.unfollow!(@followed) }.to raise_error(NoMethodError)
+    end
+    
+    it "should have a reverse_relationships method" do
+      @user.should respond_to(:reverse_relationships)
+    end
+    
+    it "should have a followers method" do
+      @user.should respond_to(:followers)
+    end
+    
+    it "should include the follower in the followers array" do
+      @user.follow!(@followed)
+      @followed.followers.should include(@user)
+    end
+    
+  end #relationships
   
 end
