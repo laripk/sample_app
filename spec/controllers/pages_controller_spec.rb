0b9a4021220d674d8dfb7417caa3482ac5f94af1
Lari Kirby <larikirby@fastmail.fm>
2011-04-30 21:37:01 -0400

    added web interface for following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,16 +8,43 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", 
-                                    :content => @base_title + " | Home")
-    end
-  end
+    
+    describe "when not signed in" do
+      
+      before(:each) do
+        get :home
+      end
+    
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title", 
+                                      :content => @base_title + " | Home")
+      end
+      
+    end #not signed in
+    
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+      
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
+      
+    end #signed in
+      
+  end #get home
 
   describe "GET 'contact'" do
     it "should be successful" do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -418,5 +418,45 @@ describe UsersController do
     end #admin
     
   end #delete destroy
-
+  
+  describe "follow pages" do
+    
+    describe "when not signed in" do
+      
+      it "should protect 'following'" do
+        get :following, :id => 1
+        response.should redirect_to(signin_path)
+      end
+      
+      it "should protect 'followers'" do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+      
+    end #not signed in
+    
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @other_user = Factory(:user, :email => Factory.next(:email))
+        @user.follow!(@other_user)
+      end
+      
+      it "should show user following" do
+        get :following, :id => @user
+        response.should have_selector("a", :href => user_path(@other_user),
+                                           :content => @other_user.name)
+      end
+      
+      it "should show user followers" do
+        get :followers, :id => @other_user
+        response.should have_selector("a", :href => user_path(@user),
+                                           :content => @user.name)
+      end
+      
+    end #signed in
+    
+  end #follow page
+  
 end #UsersController
